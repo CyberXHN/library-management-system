b486e1d24d0b332e8b9c40dc568a580f3857cf08
yhvufa <2834933035@qq.com>
2026-09-16 08:48:31 +0800

    Update BackupUtil.java

--- src/com/library/util/BackupUtil.java
+++ src/com/library/util/BackupUtil.java
@@ -4,16 +4,18 @@ import java.io.*;
 import java.util.List;
 
 public class BackupUtil {
-    // 契约要求：backup(File file, List<?> list)
-    public static void backup(File file, List<?> list) throws IOException {
+    // 契约要求：入参为文件路径String，不是File对象
+    public static void backup(String path, List<?> list) throws IOException {
+        File file = new File(path);
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
             oos.writeObject(list);
         }
     }
 
-    // 契约要求：restore(File file) 返回List
+    // 契约要求：入参为文件路径String
     @SuppressWarnings("unchecked")
-    public static <T> List<T> restore(File file) throws IOException, ClassNotFoundException {
+    public static <T> List<T> restore(String path) throws IOException, ClassNotFoundException {
+        File file = new File(path);
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
             return (List<T>) ois.readObject();
         }
