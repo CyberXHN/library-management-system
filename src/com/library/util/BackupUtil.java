0ffbaaf4c019acb735eb45ecf6e9a539956cf8b2
yhvufa <2834933035@qq.com>
2026-09-16 09:39:11 +0800

    Update BackupUtil.java

--- src/com/library/util/BackupUtil.java
+++ src/com/library/util/BackupUtil.java
@@ -4,20 +4,30 @@ import java.io.*;
 import java.util.List;
 
 public class BackupUtil {
-    // 契约要求：入参为文件路径String，不是File对象
-    public static void backup(String path, List<?> list) throws IOException {
-        File file = new File(path);
+
+    // 保留原有旧接口 File 参数！！给MainFrame继续调用，不能删掉
+    public static void backup(File file, List<?> list) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
             oos.writeObject(list);
         }
     }
 
-    // 契约要求：入参为文件路径String
+    // ✅新增重载：String路径版本，供后续新业务使用，不修改旧契约
+    public static void backup(String path, List<?> list) throws IOException {
+        backup(new File(path), list);
+    }
+
+    // 保留原有旧接口 File 参数
     @SuppressWarnings("unchecked")
-    public static <T> List<T> restore(String path) throws IOException, ClassNotFoundException {
-        File file = new File(path);
+    public static <T> List<T> restore(File file) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
             return (List<T>) ois.readObject();
         }
+    }
+
+    // ✅新增重载：String路径版本
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> restore(String path) throws IOException, ClassNotFoundException {
+        return restore(new File(path));
     }
 }
